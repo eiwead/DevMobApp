fcf4f317fbb53d6d492ddd53b2d906001f23c019
eiwead <68770731+eiwead@users.noreply.github.com>
2021-05-18 15:09:12 +0500

    Update Home.vue

--- forecapp/app/components/Home.vue
+++ forecapp/app/components/Home.vue
@@ -1,23 +1,36 @@
 <template>
     <Page>
-        <ActionBar title="Персонажи Звёздных войн"/>
-        <StackLayout>
-          <StackLayout>
-            <label> Имя: {{this.name}}</label>
-            <label> Рост: {{this.height}}</label>
-            <label> Вес: {{this.mass}}</label>
-            <label> Цвет волос: {{this.hair_color}}</label>
-            <label> Цвет кожи: {{this.skin_color}}</label>
-            <label> Цвет глаз: {{this.eye_color}}</label>
-            <label> ДР: {{this.birth_year}}</label>
-            <label> Пол: {{this.gender}}</label>
-            <label> Родина: {{this.homeworld}}</label>          
-          </StackLayout>
-          <StackLayout>
-            <TextField class="input" v-model="char_id" hint="Введите ID персонажа"  @returnPress= "newCharacter()"/>
-            <Button text='Найти' @tap= "check()"/>
-          </StackLayout>
-        </StackLayout>
+        <ActionBar title="      Персонажи Звёздных войн" text-align="center"/>
+          <TabView height="100%" android-tabs-position="bottom" selected-tab-text-color="#53ba82" tab-text-font-size="15">
+
+            <TabViewItem title="Нумерация" text-transform="uppercase">
+              <ListView for="item in characters">
+                <v-template>
+                  <Label class="info" :text="item"/>
+                </v-template>
+              </ListView>
+            </TabViewItem>
+            
+          <TabViewItem title="Описание" text-transform="uppercase">
+            <StackLayout>
+              <StackLayout>
+                <label> Имя: {{this.name}}</label>
+                <label> Рост: {{this.height}}</label>
+                <label> Вес: {{this.mass}}</label>
+                <label> Цвет волос: {{this.hair_color}}</label>
+                <label> Цвет кожи: {{this.skin_color}}</label>
+                <label> Цвет глаз: {{this.eye_color}}</label>
+                <label> Родился: {{this.birth_year}}</label>
+                <label> Пол: {{this.gender}}</label>
+                <label> Родина: {{this.homeworld}}</label>          
+             </StackLayout>
+              <StackLayout>
+               <TextField class="input" v-model="char_id" hint="Введите ID персонажа"  @returnPress= "newCharacter()"/>
+                <Button text='Найти' class="ButtFind" @tap= "check()"/>
+              </StackLayout>
+            </StackLayout>
+          </TabViewItem>
+          </TabView>
     </Page>
 </template>
 
@@ -37,6 +50,9 @@ import * as ApplicationSettings from "@nativescript/core/application-settings";
         birth_year: '', 
         gender: '',
         homeworld: '',
+        characters: ["1 - Люк Скайуокер", "2 - C-3PO", "3 - R2-D2", 
+                    "4 - Дарт Вейдер", "5 - Лея Органа", "6 - Оуэн Ларс", "7 - Беру Уайтсан Ларс",
+                    "8 - R5-D4", "9 - Биггс Дарклайтер", "10 - Оби-Ван Кеноби"]
       }
     
     },
@@ -75,6 +91,14 @@ ActionBar {
   color: yellow;
   }
   .text{
-      text-align: center;
+    text-align: center;
+  }
+  .ButtFind{
+    color: yellow;
+    background-color: black;
+  }
+  .ButtFind:active{
+    color: black;
+    background-color: yellow;
   }
 </style>
