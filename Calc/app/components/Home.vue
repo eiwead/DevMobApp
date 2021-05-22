f99b35d0b8514c870ac3b19781020e7b520f1bc3
eiwead <68770731+eiwead@users.noreply.github.com>
2021-05-22 13:03:30 +0500

    Update Home.vue

--- Calc/app/components/Home.vue
+++ Calc/app/components/Home.vue
@@ -2,31 +2,32 @@
     <Page  class="Application">
          <GridLayout class= "Symbols" columns= "*, *, *, *" rows= "*, *, *, *, *, *">
             <Label textWrap= "true" class= "Output" v-model="output" row="0" col="0" colSpan="4"/>
-            <Button  class="button delete" text="C" @tap="del()" row="1" col="0" colSpan="3"/>
-            <Button  class="button ops" text="÷" @tap="op('/')" row="1" col="3"/>
-            <Button  class="button" text="7" @tap="input('7')" row="2" col="0"/>
-            <Button  class="button" text="8" @tap="input('8')" row="2" col="1"/>
-            <Button  class="button" text="9" @tap="input('9')" row="2" col="2"/>
-            <Button  class="button ops" text="x" @tap="op('*')" row="2" col="3"/>
-            <Button  class="button" text="4" @tap="input('4')" row="3" col="0"/>
-            <Button  class="button" text="5" @tap="input('5')" row="3" col="1"/>
-            <Button  class="button" text="6" @tap="input('6')" row="3" col="2"/>
-            <Button  class="button ops" text="-" @tap="op('-')" row="3" col="3"/>
-            <Button  class="button" text="1" @tap="input('1')" row="4" col="0"/>
-            <Button  class="button" text="2" @tap="input('2')" row="4" col="1"/>
-            <Button  class="button" text="3" @tap="input('3')" row="4" col="2"/>
-            <Button  class="button ops" text="+" @tap="op('+')" row="4" col="3"/>
-            <Button  class="button" text="0" @tap="input('0')" row="5" col="0" colSpan = "2"/>
-            <Button  class="button" text="." @tap="dot('.')" row="5" col="2"/>
-            <Button  class="button ops" text="=" @tap="calc()" row="5" col="3"/>
+            <Button  class="button delete"  @tap="del()"      text="C" row="1" col="0" colSpan="3"/>
+            <Button  class="button ops"     @tap="op('/')"    text="÷" row="1" col="3"/>
+            <Button  class="button"         @tap="input('7')" text="7" row="2" col="0"/>
+            <Button  class="button"         @tap="input('8')" text="8" row="2" col="1"/>
+            <Button  class="button"         @tap="input('9')" text="9" row="2" col="2"/>
+            <Button  class="button ops"     @tap="op('*')"    text="x" row="2" col="3"/>
+            <Button  class="button"         @tap="input('4')" text="4" row="3" col="0"/>
+            <Button  class="button"         @tap="input('5')" text="5" row="3" col="1"/>
+            <Button  class="button"         @tap="input('6')" text="6" row="3" col="2"/>
+            <Button  class="button ops"     @tap="op('-')"    text="-" row="3" col="3"/>
+            <Button  class="button"         @tap="input('1')" text="1" row="4" col="0"/>
+            <Button  class="button"         @tap="input('2')" text="2" row="4" col="1"/>
+            <Button  class="button"         @tap="input('3')" text="3" row="4" col="2"/>
+            <Button  class="button ops"     @tap="op('+')"    text="+" row="4" col="3"/>
+            <Button  class="button"         @tap="input('0')" text="0" row="5" col="0" colSpan = "2"/>
+            <Button  class="button"         @tap="dot('.')"   text="." row="5" col="2"/>
+            <Button  class="button ops"     @tap="calc()"     text="=" row="5" col="3"/>
          </GridLayout>
     </Page>
 </template>
 
 <script >
-    let digit = false;
-    let dot = false;
-    let ow = true
+let digit =  false;
+let dot = false;
+let ow = true;
+
    export default  {
     data() {
         return {
@@ -64,7 +65,7 @@
                         }
                         digit = true;
                         dot = true;
-                        his.output += char;
+                        this.output += char;
                     }                
                 },
                 
